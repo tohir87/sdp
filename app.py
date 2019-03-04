9080276171d157af4d63969bd97467b4b411fc9f
Tohir Omoloye <otcleantech@gmail.com>
2019-03-04 18:27:43 +0000

    Added static files

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path=os.getcwd() + 'templates/vendor')
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
-def hello():
-    return "Hello world!"
+def home():
+    return render_template("setup.html")
 
 
 @app.route('/about')
 def about():
-    return "Developed by Thohiru Omoloye <br> 2950574"
+    return jsonify({'name': "Thohiru Omoloye", 'student No.': "2950574"})
 
 
 if __name__ == '__main__':
