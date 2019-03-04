75366d38a422239b0582261ffa69731c68662aa2
Tohir Omoloye <otcleantech@gmail.com>
2019-03-04 11:59:55 +0000

    added config file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sdp
 final year project
+
+#### App Settings
+`export APP_SETTINGS="config.DevelopmentConfig"`
+
+#### Heroku Settings - Stagging
+`heroku config:set APP_SETTINGS=config.StagingConfig --remote stage
+`
+#### Heroku Settings - Production
+`heroku config:set APP_SETTINGS=config.ProductionConfig --remote pro
+`
--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def hello():
     return "Hello world!"
+
+
+@app.route('/about')
+def about():
+    return "Developed by Thohiru Omoloye <br> 2950574"
+
 
 if __name__ == '__main__':
     app.run()
