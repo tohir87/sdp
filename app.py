6252e684ec55024bc76956ef75423bae1d96016d
Tohir Omoloye <otcleantech@gmail.com>
2019-03-14 16:41:37 +0000

    updates

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
 import os
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from classes.farm import Farm
+import psycopg2
+from config import config
 
 app = Flask(__name__, static_url_path=os.getcwd() + 'templates/vendor')
 app.config.from_object(os.environ['APP_SETTINGS'])
 print(os.environ['APP_SETTINGS'])
 
 
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        return psycopg2.connect(**params)
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
 @app.route('/setup')
 def home():
+    conn = connect()
+    # create a cursor
+    cur = conn.cursor()
+
+    # execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchone()
+    print(db_version)
+
+    # close connection
+    conn.close()
     return render_template("setup.html")
 
 
@@ -36,11 +71,6 @@ def doSetup():
     farm = Farm(request.form)
     print("response")
     print(farm.signUp())
-    # first_name = request.form['first_name']
-    # last_name = request.form['last_name']
-    # email = request.form['email']
-    # phone = request.form['phone']
-    # password = request.form['password']
 
     # complete setup process
 
