4ec46fe6220eed1cc8123fdb8c3fb999abd51e76
Tohir Omoloye <otcleantech@gmail.com>
2019-03-22 00:03:05 +0000

    added database migration

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ migrate = Migrate(app, db)
 
 
 class User(db.Model):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(128))
     last_name = db.Column(db.String(128))
