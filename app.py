5a50b4c5bebf647a0cc7f71c2de9b7690fa669ad
Tohir Omoloye <otcleantech@gmail.com>
2019-04-20 17:15:06 +0100

    bug fix

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def get_temp_humid():
         # build data
         for row in dht_data:
             temp.append(float(row[3]))
-            humid.append(ifloatnt(row[4]))
+            humid.append(float(row[4]))
             dates.append(row[1])
 
     return jsonify({'temperature': temp, 'humidity': humid, 'categories': dates})
